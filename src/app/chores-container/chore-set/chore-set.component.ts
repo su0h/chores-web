0314ca84459f7a01edefd650dbbc7db9a1323621
Andre Ponce <andre_ponce@dlsu.edu.ph>
2023-12-22 12:19:33 +0800

    Created basic layout

--- src/app/chores-container/chore-set/chore-set.component.ts
+++ src/app/chores-container/chore-set/chore-set.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { ChoreService } from '../chore.service';
 
 @Component({
   selector: 'app-chore-set',
@@ -7,6 +8,19 @@ import { Component } from '@angular/core';
   templateUrl: './chore-set.component.html',
   styleUrl: './chore-set.component.css'
 })
-export class ChoreSetComponent {
+export class ChoreSetComponent implements OnInit {
+  protected assignees: Array<String>;
+  protected tasks: Array<String>;
+  @Input() header: String;
+  
+  constructor(protected choreService: ChoreService) {
+    this.assignees = [];
+    this.tasks = [];
+    this.header = '';
+  }
 
+  ngOnInit(): void {
+      this.assignees = this.choreService.assignees;
+      this.tasks = this.choreService.tasks;
+  }
 }
---
+++ src/app/chores-container/chore.service.ts
@@ -0,0 +1,40 @@
+import { Injectable, OnInit } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ChoreService {
+  private _assignees: Array<String> = ["Person 1", "Person 2", "Person 3"];
+  private _tasks: Array<String> = ["Hain", "Ligpit", "Hugas"];
+
+  constructor() { 
+    const day_num = 1;
+    this._assignees = this.rotateArray(this._assignees, day_num);
+    console.log(this._assignees);
+  }
+
+  public get assignees(): Array<String> {
+    return [...this._assignees];
+  }
+
+  public get tasks(): Array<String> {
+    return [...this._tasks];
+  }
+
+  private rotateArray(arr: Array<String>, positions: number): Array<String> {
+    const length = arr.length;
+
+    // Ensure a valid number of positions
+    positions = positions % length;
+  
+    // Handle negative positions
+    if (positions < 0) {
+      positions = length + positions;
+    }
+  
+    // Rotate the array
+    const rotatedArray = [...arr.slice(-positions), ...arr.slice(0, length - positions)];
+  
+    return rotatedArray;
+  }
+}
