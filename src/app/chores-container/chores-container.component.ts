c97c60642dcf3b6b56e5e2e4418e3083f7a00cc9
Andre Ponce <andre_ponce@dlsu.edu.ph>
2024-01-19 14:01:31 +0800

    Removed chores-set component

--- src/app/chores-container/chores-container.component.ts
+++ src/app/chores-container/chores-container.component.ts
@@ -1,13 +1,26 @@
-import { Component } from '@angular/core';
-import { ChoreSetComponent } from './chore-set/chore-set.component';
+import { Component, Input, OnInit } from '@angular/core';
+import { ChoreService } from './chore.service';
 
 @Component({
   selector: 'app-chores-container',
   standalone: true,
-  imports: [ChoreSetComponent],
+  imports: [],
   templateUrl: './chores-container.component.html',
   styleUrl: './chores-container.component.css'
 })
-export class ChoresContainerComponent {
+export class ChoresContainerComponent implements OnInit {
+  protected assignees: Array<String>;
+  protected tasks: Array<String>;
+  @Input() header: String;
 
+  constructor(protected choreService: ChoreService) {
+    this.assignees = [];
+    this.tasks = [];
+    this.header = '';
+  }
+
+  ngOnInit(): void {
+    this.assignees = this.choreService.assignees;
+    this.tasks = this.choreService.tasks;
+  }
 }
