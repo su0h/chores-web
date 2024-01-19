b1bde08fbe4e1e4dd0d5ab4f6d323453c3ed2915
Andre Ponce <andre_ponce@dlsu.edu.ph>
2024-01-19 21:19:46 +0800

    More UI refinements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,14 +3,22 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { ChoresContainerComponent } from './chores-container/chores-container.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faUndo } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent, ChoresContainerComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, ChoresContainerComponent, FontAwesomeModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'chores';
+  faUndo = faUndo;
+
+  protected revertTaskAssignments(): void {
+    // TODO: Implement this functionality (API call to backend to unshift)
+    console.log("TODO");
+  }
 }
