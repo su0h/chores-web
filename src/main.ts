a72de2739f07825f575ea69b92c9b244adeb8ee4
Andre Ponce <andre_ponce@dlsu.edu.ph>
2024-01-26 20:11:40 +0800

    Added environments

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,13 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { environment } from './environments/environment';
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
+
+if (environment.production) {
+  console.log("Environment is running in production mode!");
+} else {
+  console.log("Environment is running in dev mode!");
+}
