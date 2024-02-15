974718dac2e32fdfbbd1ade6ecc588d497b386be
Andre Ponce <andre_ponce@dlsu.edu.ph>
2024-02-15 20:09:53 +0800

    Reverted local deployment IP address

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 // Reference: https://www.digitalocean.com/community/tutorials/angular-environment-variables
 export const environment = {
     production: true, 
-    API_URL: "http://192.168.1.92:8080/api/v1.0/task-assignments"
+    API_URL: "http://localhost:8080/api/v1.0/task-assignments"
 }
