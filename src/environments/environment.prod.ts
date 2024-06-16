4940f0f25e2b1321966954126710fb5fc07619d0
Andre Ponce <ndr.hbk@gmail.com>
2024-06-16 21:35:07 +0800

    Added API_URL environment variable

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 // Reference: https://www.digitalocean.com/community/tutorials/angular-environment-variables
 export const environment = {
     production: true, 
-    API_URL: "http://localhost:8080/api/v1.0/task-assignments"
+    API_URL: "${API_URL}"
 }
